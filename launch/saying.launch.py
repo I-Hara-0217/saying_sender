247f39d361d145dc28c1262c55303a4d78446a74
I.Hara <i.hara.git@gmail.com>
2025-12-30 07:09:45 +0900

    体裁の整理

--- launch/saying.launch.py
+++ launch/saying.launch.py
@@ -1,15 +1,17 @@
+# SPDX-FileCopyrightText: 2025 Ibuki Hara
+# SPDX-License-Identifier: BSD-3-Clause
+
 import launch
 import launch_ros.actions
 
+
 def generate_launch_description():
     return launch.LaunchDescription([
-        # 送信機の起動
         launch_ros.actions.Node(
             package='saying_sender',
             executable='quotes_publisher',
             name='publisher_node'
         ),
-        # 受信機の起動
         launch_ros.actions.Node(
             package='saying_sender',
             executable='quotes_subscriber',
