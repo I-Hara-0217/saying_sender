af318853fc52b379a252aeb27166bda53b88f7f6
I.Hara <i.hara.git@gmail.com>
2025-12-30 07:58:44 +0900

    元に戻す

--- test.bash
+++ test.bash
@@ -2,12 +2,7 @@
 # SPDX-FileCopyrightText: 2025 Ibuki Hara
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-    res=1
-}
-
 res=0
-
 export PYTHONUNBUFFERED=1
 export RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED=1
 
@@ -19,9 +14,7 @@ source install/setup.bash
 timeout 25s ros2 run saying_sender quotes_publisher > /tmp/saying_sender.log 2>&1 || true
 
 count=$(grep -c "Publish:" /tmp/saying_sender.log)
-
-[ "$count" -ge 3 ] || ng "$LINENO"
+[ "$count" -ge 3 ] || res=1
 
 [ "$res" = 0 ] && echo OK
-
 exit $res
